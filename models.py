497637838985aa1f158ef966da622163c8d758b2
emilio4585 <emiliosalas54@gmail.com>
2021-02-16 20:46:26 -0600

    mejoras 2

--- models.py
+++ models.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from modulo import db
 
-class users(db.Model):
-    _id = db.Column("id", db.Integer, primary_key = True)
+class User(db.Model):
+    __id = db.Column("id", db.Integer, primary_key = True, )
     name = db.Column(db.String(100))
     email = db.Column(db.String(100))
     address = db.Column(db.String(200))

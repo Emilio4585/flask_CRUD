c79b9de9a3290c8e60b63f518e36536f4202776a
emilio4585 <emiliosalas54@gmail.com>
2021-02-16 22:44:35 -0600

    mejoras 3

--- app.py
+++ app.py
@@ -7,56 +7,37 @@ from modulo import app,db
 
 @app.route("/", methods=["POST", "GET"])
 def home():
-
     all_data = User.query.all()
-
-    
     return render_template("index.html", users = all_data)
-
 
 @app.route('/new', methods=["POST", "GET"])
 def create_user():
-    
-
     if request.method == "POST":
         try:
             name = request.form.get("inputname")
             email= request.form.get("inputemail")
             address = request.form.get("inputAddress")
             phone = request.form.get("inputphone")
-
             user = User(name=name, email=email, address=address,phone=phone)
-
             user.add()
-
         except Exception as e:
             print("Fallo al añadir usuario")
             print(e)
-  
-  
     return redirect(url_for('home'))
-
-
 
 @app.route("/update", methods=['POST','GET'])
 def update_user():
-
     if request.method == "POST":
-
         try:
-
-            iD = User.query.get(request.form.get("id"))
-            iD.name = request.form.get("editname")
-            iD.email= request.form.get("editemail")
-            iD.address = request.form.get("editaddress")
-            iD.phone = request.form.get("editphone")
-
-            iD.update()
-        
+            id = User.query.get(request.form.get("id"))
+            id.name = request.form.get("editname")
+            id.email= request.form.get("editemail")
+            id.address = request.form.get("editaddress")
+            id.phone = request.form.get("editphone")
+            id.update()
         except Exception as e:
             print("Fallo al actualizar el user")
             print(e)
-    
     return redirect(url_for("home"))
 
 

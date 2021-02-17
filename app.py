0b0e1dcda6a296bf7be55008f130330d91572996
Emily <emily.moraima.cruz@hotmail.com>
2021-02-17 00:23:12 -0500

    espacios

--- app.py
+++ app.py
@@ -3,37 +3,25 @@ from flask_sqlalchemy import SQLAlchemy
 from models import User
 from modulo import app,db
 
-
-
 @app.route("/", methods=["POST", "GET"])
 def home():
-
     all_data = User.query.all()
-
-    
     return render_template("index.html", users = all_data)
 
 
 @app.route('/new', methods=["POST", "GET"])
 def create_user():
-    
-
     if request.method == "POST":
         try:
             name = request.form.get("inputname")
             email= request.form.get("inputemail")
             address = request.form.get("inputAddress")
             phone = request.form.get("inputphone")
-
             user = User(name=name, email=email, address=address,phone=phone)
-
             user.add()
-
         except Exception as e:
             print("Fallo al añadir usuario")
             print(e)
-  
-  
     return redirect(url_for('home'))
 
 
